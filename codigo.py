73dbdb7952e056f6e5d6b0efc0c0637b55d87842
jone <jvegadel@myuax.com>
2022-12-08 21:35:36 +0100

    d

--- codigo.py
+++ codigo.py
@@ -20,17 +20,22 @@ def recorre_laberinto(laberinto):
         if movimientos[-1] != 'Arriba' and fila + 1 < n and laberinto[fila + 1][columna] != 'X':
             fila += 1
             movimientos.append('Abajo')
+            print("abajo")
         elif movimientos[-1] != 'Abajo' and fila - 1 > 0 and laberinto[fila - 1][columna] != 'X':
             fila -= 1
             movimientos.append('Arriba')
+            print("arriba")
         elif movimientos[-1] != 'Izquierda' and columna + 1 < n and laberinto[fila][columna + 1] != 'X':
             columna += 1
             movimientos.append('Derecha')
+            print("derecha")
         elif movimientos[-1] != 'Derecha' and columna - 1 > 0 and laberinto[fila][columna - 1] != 'X':
             columna -= 1
             movimientos.append('Izquierda')
+            print("izquierda")
         else:
             movimientos.append('No hay salida')
+            break
     return movimientos
 
 print('La solucion es:', recorre_laberinto)
